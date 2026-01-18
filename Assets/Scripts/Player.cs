20f38e6d0ba66f4d658a0671196766e3adc50c25
leiyu0803 <leiyu0803@qq.com>
2026-01-18 12:05:06 -0800

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -89,8 +89,8 @@ public class Player : MonoBehaviour
     {
         _dayBGM = GameObject.Find("DayBGM").GetComponent<AudioSource>();
         _nightBGM = GameObject.Find("NightBGM").GetComponent<AudioSource>();
-        //_dayBGM.volume = 0.3f;
-        //_nightBGM.volume = 0.0f;
+        _dayBGM.volume = 0.0f;
+        _nightBGM.volume = 0.0f;
         _sessionTime = _maxSessionTime;
         _reachSystem.transform.localScale = new Vector3(_reachRange, _reachRange, _reachRange);
         _moveSpeed = _speed * 10;
